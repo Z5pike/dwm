1c97709f10eb514fd02381bf70fd554d1dab0bd7
Z5pike <dumspike@gmail.com>
2022-03-27 20:58:48 +0800

    set float

--- config.def.h
+++ config.def.h
@@ -38,10 +38,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "icalingua",  NULL,       NULL,       1 << 2,     1,           -1 },
+	/* class                          instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",                         NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",                      NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "icalingua",                    NULL,       NULL,       1 << 2,       1,           -1 },
+	{ "netease-cloud-music",          NULL,       NULL,       1 << 3,       1,           -1 },
+  
 };
 
 /* layout(s) */
